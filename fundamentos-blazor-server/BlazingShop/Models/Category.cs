7efd23b625572d8a4876f65e2b9b2bbc05c07d96
betogzo <albertogaleazzo@msn.com>
2024-01-12 14:32:52 -0300

    basic usage of layouts,components,parameters etc

---
+++ fundamentos-blazor-server/BlazingShop/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazingShop.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            
+        }
+
+        public Category(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ fundamentos-blazor-server/BlazingShop/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace BlazingShop.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(int id, string name, string description, int categoryId, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            //Category = category;
+            CategoryId = categoryId;
+            Price = price;
+        }
+
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        //public Category Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
