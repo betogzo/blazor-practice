f410139ee62ce358aa5510938a8f3dac9417b8e0
betogzo <albertogaleazzo@msn.com>
2024-01-31 12:58:09 -0300

    add data annotations

--- fundamentos-blazor-net8/BlazorShop/Models/Category.cs
+++ fundamentos-blazor-net8/BlazorShop/Models/Category.cs
@@ -1,7 +1,14 @@
-﻿namespace BlazorShop.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShop.Models;
 
 public class Category
 {
+    [Key]
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Category title is required.")]
+    [MinLength(3, ErrorMessage = "Category title must have at least 3 characters.")]
+    [MaxLength(60, ErrorMessage = "Category title can't be longer than 60 characters.")]
     public string Title { get; set; } = string.Empty;
 }
--- fundamentos-blazor-net8/BlazorShop/Models/Product.cs
+++ fundamentos-blazor-net8/BlazorShop/Models/Product.cs
@@ -1,12 +1,23 @@
-﻿namespace BlazorShop.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShop.Models;
 
 public class Product
 {
+    [Key]
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Product title is required.")]
+    [MinLength(3, ErrorMessage = "Product title must have at least 3 characters.")]
+    [MaxLength(120, ErrorMessage = "Product title can't be longer than 120 characters.")]
     public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+
+    public string? Description { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Product value is required.")]
+    [DataType(DataType.Currency)]
     public decimal Price { get; set; }
+
     public int CategoryId { get; set; }
     public Category Category { get; set; } = null!;
 }
-
